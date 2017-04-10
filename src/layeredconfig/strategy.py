1947acc37d4f45a6b1969edfe3f7f10065e647b2
Philipp Busch <hakkeroid@philippbusch.de>
2017-04-10 23:36:15 +0200

    Rename misleading new to next

--- src/layeredconfig/strategy.py
+++ src/layeredconfig/strategy.py
@@ -1,16 +1,16 @@
 # -*- coding: utf-8 -*-
 
-def add(new, previous=None):
+def add(next_, previous=None):
     if previous is None:
-        return new
-    return previous + new
+        return next_
+    return previous + next_
 
 
-def collect(new, previous=None):
+def collect(next_, previous=None):
     if previous is None:
-        return [new]
-    return previous + [new]
+        return [next_]
+    return previous + [next_]
 
 
-def merge(new, previous=None):
-    return add(new, previous)
+def merge(next_, previous=None):
+    return add(next_, previous)
